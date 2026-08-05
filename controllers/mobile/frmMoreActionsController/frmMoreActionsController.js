93b80a5eeb0ac7c7b052ee072eb2c368bd101d27
Mohammad Raza <tajdar.tr@gmail.com>
2026-08-05 14:53:34 +0300

    fix

--- controllers/mobile/frmMoreActionsController/frmMoreActionsController.js
+++ controllers/mobile/frmMoreActionsController/frmMoreActionsController.js
@@ -4,7 +4,7 @@ define({
     onNavigate: function (params) {
         this.view.init = this.onInit;
         this.view.preShow = this.preShow;
-        this.view.onDeviceBack = this.onDeviceBack;
+        //this.view.onDeviceBack = this.onDeviceBack;
     },
 
     onInit: function () {
